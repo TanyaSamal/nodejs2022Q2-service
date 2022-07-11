80e0bc825fc638b2b5564558789a40b97d66a35b
Tanya Samal <solesytto@gmail.com>
2022-07-11 22:38:57 +0300

    init: start nodejs-service project, add swagger and config port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { dirname, join, resolve } from 'path';
+import { parse } from 'yaml';
+import { readFile } from 'fs/promises';
+import { cwd } from 'process';
+import { config } from 'dotenv';
+
+config({ path: resolve(cwd(), '.env') });
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
+
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(PORT);
 }
+
 bootstrap();
