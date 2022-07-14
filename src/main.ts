39cbae1bce7cce1016d2c0d7a1396247dea17733
Tanya Samal <solesytto@gmail.com>
2022-07-14 23:08:36 +0300

    dock: change swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { dirname, join, resolve } from 'path';
 import { parse } from 'yaml';
 import { readFile } from 'fs/promises';
 import { cwd } from 'process';
 import { config } from 'dotenv';
-import { ValidationPipe } from '@nestjs/common';
 
 config({ path: resolve(cwd(), '.env') });
 
@@ -15,18 +14,11 @@ const PORT = process.env.PORT || 4000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Home Library Service')
-    .setDescription('Home music library service')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const rootDirname = dirname(__dirname);
+  const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
+  const document = parse(DOC_API);
 
-  // const rootDirname = dirname(__dirname);
-  // const DOC_API = await readFile(join(rootDirname, 'doc', 'api.yaml'), 'utf-8');
-  // const document = parse(DOC_API);
-
-  // SwaggerModule.setup('doc', app, document);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(PORT);
 }
